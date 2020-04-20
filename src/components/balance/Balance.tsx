386327b50c633fc1466d08a2023d2659d155d6c2
Mohit Tanwani <mohitmurlidhar@gmail.com>
2020-04-20 21:40:50 +0530

    :fire: Added Balance Component

---
+++ src/components/balance/Balance.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+
+export const Balance = () => {
+    return (
+        <>
+          <h4>Your Balance</h4>
+          <h1>$0.00</h1>  
+        </>
+    )
+}
