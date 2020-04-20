ffb16a7d0ced0ee88b0e8411a8522d2beb9aa743
Mohit Tanwani <mohitmurlidhar@gmail.com>
2020-04-21 00:39:33 +0530

    :bug: Bug Fixes

--- src/components/addTransaction/Addtransaction.tsx
+++ src/components/addTransaction/Addtransaction.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import './style.css'
 export const Addtransaction = () => {
+    const [text,setText] = React.useState("");
+    const [amount,setAmount] = React.useState(0);
     return (
         <>
             <h3>Add Transaction</h3>
             <form id="form">
                 <div className="form-control">
                     <label htmlFor="text" >Text</label>
-                    <input type="text" id="text" placeholder="Enter text..." />
+                    <input type="text" id="text" value={text} onChange={(e)=>setText(e.target.value)} placeholder="Enter text..." />
                 </div>
                 <div className="form-control">
                     <label htmlFor="amount">Amount <br />
             (negative - expense, positive - income)</label
                     >
-                    <input type="number" id="amount" placeholder="Enter amount..." />
+                    <input type="number" id="amount" value={amount} onChange={(e) => setAmount(parseInt(e.target.value)) } placeholder="Enter amount..." />
                 </div>
                 <button className="btn">Add transaction</button>
             </form>
