e0c0601605ee0441590facc851e288752d208c3d
Mohit Tanwani <mohitmurlidhar@gmail.com>
2020-04-30 19:30:57 +0530

    :bug: Bug fixes

--- client/src/components/incomeExpenses/IncomeExpenses.tsx
+++ client/src/components/incomeExpenses/IncomeExpenses.tsx
@@ -8,13 +8,13 @@ export const IncomeExpenses = (props: IProps) => {
     const { transactions } = React.useContext(GlobalContext);
     let date = new Date();
     let firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
-    let lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+    let lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 1);
     const monthNames = ["January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"
     ];
     console.log(firstDay, " ", lastDay);
-    const income = transactions.filter(item => item.amount > 0).filter(item => (item.createdAt ? new Date(item.createdAt) : "") > firstDay && (item.createdAt ? new Date(item.createdAt) : "") <= lastDay).reduce((amt, item) => (amt += item.amount), 0).toFixed(2);
-    const expense = (transactions.filter(item => item.amount < 0).filter(item => (item.createdAt ? new Date(item.createdAt) : "") > firstDay && (item.createdAt ? new Date(item.createdAt) : "") <= lastDay).reduce((amt, item) => (amt += item.amount), 0) * (-1)).toFixed(2);
+    const income = transactions.filter(item => item.amount > 0).filter(item => (item.createdAt ? new Date(item.createdAt) : "") > firstDay && (item.createdAt ? new Date(item.createdAt) : "") < lastDay).reduce((amt, item) => (amt += item.amount), 0).toFixed(2);
+    const expense = (transactions.filter(item => item.amount < 0).filter(item => (item.createdAt ? new Date(item.createdAt) : "") > firstDay && (item.createdAt ? new Date(item.createdAt) : "") < lastDay).reduce((amt, item) => (amt += item.amount), 0) * (-1)).toFixed(2);
     return (
         <>
             <h5 className="ie-heading">{monthNames[date.getMonth()]} Month's </h5>
